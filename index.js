ba469cc79b62d7ba3e9884423217be101161039a
Jakub Sztandera <kubuxu@gmail.com>
2016-01-18 15:56:35 +0100

    Inital commit

---
+++ index.js
@@ -0,0 +1,86 @@
+
+
+'use strict'
+
+var http = require('http')
+var httpProxy = require('http-proxy')
+
+var proxy = httpProxy.createProxyServer()
+proxy.on('error', (err) => {
+  // There has to be error handler.
+  console.log('Proxy error:', err)
+})
+var pool = [
+  {
+    host: 'v03x.ipfs.io',
+    port: 80
+  },
+  {
+    host: 'v04x.ipfs.io',
+    port: 80
+  }
+]
+var allowedCodes = {
+  200: true,
+  302: true,
+  304: true
+}
+
+// TODO: Caching.
+// /ipfs/ - 60 minutes TTL
+// /ipns/ - 1 minute TTL
+// this includes caching requests to non /ip(n|f)s/ where Host
+// header specifies /ipfs/${host}
+var cache = new require( "node-cache" )({
+  useClones: false
+})
+
+function resolve(req, res) {
+  var done = false
+  var running = 0
+  console.log('Got request.')
+  for (var i = 0; i < pool.length; i++) {
+    running++
+    console.log('Checking', req.url, 'to:', pool[i].host)
+    var head = http.request({
+      host: pool[i].host,
+      port: pool[i].port,
+      path: req.url,
+      headers: {
+        host: req.headers.host,
+        connection: 'keep-alive'
+      },
+      method: 'HEAD'
+    }, ((server, r) => {
+      running--
+      if (done) {
+        return
+      }
+      if (allowedCodes[r.statusCode]) {
+        done = true
+        console.log('Sucess', req.url, 'at', server.host)
+        proxy.web(req, res, { target: server })
+      } else {
+        console.log('Failed', req.url, 'at', server.host, r.statusCode)
+        // Recheck `done` because of raceconditon
+        if (!done && running === 0) {
+          done = true
+          res.write('Could not find it anywhere')
+          res.statusCode = 400
+          res.end()
+        }
+      }
+    }).bind(null, pool[i]))
+    head.on('error', ((server, e) => {
+      // There has to be error handler.
+      console.log('Error on: ', server.host, e.message)
+    }).bind(null, pool[i]))
+    head.end()
+  }
+}
+
+http.createServer(function (req, res) {
+  if (req.url.match(/\/(ipfs)|(ipns)\//)) {
+
+  }
+}).listen(8082)
---
+++ readme.md
@@ -0,0 +1,45 @@
+# ipfs-gateway-proxy [![Build Status](https://travis-ci.org/Kubuxu/ipfs-gateway-proxy.svg?branch=master)](https://travis-ci.org/Kubuxu/ipfs-gateway-proxy)
+
+> Multi gateway proxy for ipfs
+
+
+## Install
+
+```
+$ npm install --save ipfs-gateway-proxy
+```
+
+
+## Usage
+
+```js
+const ipfsGatewayProxy = require('ipfs-gateway-proxy');
+
+ipfsGatewayProxy('unicorns');
+//=> 'unicorns & rainbows'
+```
+
+
+## API
+
+### ipfsGatewayProxy(input, [options])
+
+#### input
+
+Type: `string`
+
+Lorem ipsum.
+
+#### options
+
+##### foo
+
+Type: `boolean`  
+Default: `false`
+
+Lorem ipsum.
+
+
+## License
+
+MIT © [Jakub Sztandera](http://ipfs.io/ipns/ipns.kubuxu.ovh)
